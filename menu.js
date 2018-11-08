7ea83bd3d93c131e58527398c0f20755c1814f9b
cassieflorido <cgf7xf@mail.missouri.edu>
2018-11-08 13:20:25 -0600

    hamburger menu

--- menu.js
+++ menu.js
@@ -1,1 +1,11 @@
+$(document).ready(function() {
+  $('.menutrigger').click(function() {
+    $('.item').slideToggle(500);
+  });
 
+  $(window).resize(function() {
+	   if ($(window).width()>500) {
+		$('.item').removeAttr('style');
+	 }
+	});
+});
